2f4bff5d49eeefb578ae174cdf205830cab45b13
Juce <iamjuceuwu@gmail.com>
2025-01-09 19:41:25 +0000

    Move IJobContext to it's own file

---
+++ Cronjure/IJobContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Cronjure;
+
+public interface IJobContext
+{
+    CancellationToken CancellationToken { get; set; }
+    IDictionary<string, object>? Data { get; set; }
+}
--- Cronjure/JobContext.cs
+++ Cronjure/JobContext.cs
@@ -1,11 +1,5 @@
 ﻿namespace Cronjure;
 
-public interface IJobContext
-{
-    CancellationToken CancellationToken { get; set; }
-    IDictionary<string, object>? Data { get; set; }
-}
-
 public class JobContext : IJobContext
 {
     public CancellationToken CancellationToken { get; set; }
